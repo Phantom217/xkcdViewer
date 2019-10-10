781c4c4991233ab0780efd5a1f0e98a7ca593fec
Tyler Oalman <tyleroalman@gmail.com>
2019-10-10 13:29:18 -0500

    Add library versions

---
+++ project/Versions.scala
@@ -0,0 +1,11 @@
+object Versions {
+
+  val appV = "1.0.0"
+  val scalaV = "2.11.8"
+  val androidPlatformV = "android-24"
+  val androidV = "25.0.1"
+  val macroidV = "2.0"
+  val specs2V = "2.4.15"
+  val mockitoV = "3.0-M2"
+
+}
