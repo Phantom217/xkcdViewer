7a17a7290ea82ca153080532f7abbd1d6991aaa3
Tyler Oalman <tyleroalman@gmail.com>
2019-10-12 17:47:00 -0500

    Update manifest and MainActivity

--- src/main/scala/com/paoerful/android/xkcd/MainActivity.scala
+++ src/main/scala/com/paoerful/android/xkcd/MainActivity.scala
@@ -1,22 +1,45 @@
 package com.paoerful.android.xkcd
 
+import scala.language.postfixOps
+
 import android.app.Activity
 import android.os.Bundle
+import android.widget.{ LinearLayout, Button, TextView }
 import android.support.v7.app.AppCompatActivity
 import android.graphics.drawable.Animatable
+import macroid._
+import macroid.Contexts
+import macroid.FullDsl._
 
-class MainActivity extends AppCompatActivity {
-    // allows accessing `.value` on TR.resource.constants
-    implicit val context = this
 
-    override def onCreate(savedInstanceState: Bundle): Unit = {
-        super.onCreate(savedInstanceState)
-        // type ascription is required due to SCL-10491
-        val vh: TypedViewHolder.main = TypedViewHolder.setContentView(this, TR.layout.main)
-        vh.text.setText(s"Hello world, from ${TR.string.app_name.value}")
-        vh.image.getDrawable match {
-          case a: Animatable => a.start()
-          case _ => // not animatable
-        }
+class MainActivity
+  extends Activity
+  with Contexts[Activity]
+{
+  // allows accessing `.value` on TR.resource.constants
+  // implicit val context = this
+
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+
+    setContentView {
+      Ui.get {
+        l[LinearLayout](
+          w[TextView]
+        )
+        l[LinearLayout](
+          w[Button] <~ text("Hello!") <~ padding(top = 8 dp, bottom = 8 dp),
+          w[TextView]
+        )
+      }
     }
+
+    // val vh: TypedViewHolder.main =
+    //   TypedViewHolder.setContentView(this, TR.layout.main)
+    // vh.text.setText(s"Hello world, from ${TR.string.app_name.value}")
+    // vh.image.getDrawable match {
+    //   case a: Animatable => a.start()
+    //   case _             => // not animatable
+    // }
+  }
 }
