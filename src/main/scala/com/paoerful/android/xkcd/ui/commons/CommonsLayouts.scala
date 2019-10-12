32cdf867291c406510e351854cc65a976013c5df
Tyler Oalman <tyleroalman@gmail.com>
2019-10-12 17:48:04 -0500

    Add common layout

---
+++ src/main/scala/com/paoerful/android/xkcd/ui/commons/CommonsLayouts.scala
@@ -0,0 +1,29 @@
+package com.paoerful.android.xkcd.ui.commons
+
+import android.support.v7.widget.RecyclerView
+import android.widget.{ LinearLayout, TextView }
+import macroid.FullDsl._
+import macroid.{ ActivityContextWrapper, Ui }
+
+class HeaderLayoutAdapter(implicit ctx: ActivityContextWrapper)
+  extends HeaderAdapterStyles
+{
+  var headerName: Option[TextView] = slot[TextView]
+
+  val content: LinearLayout = layout
+
+  private def layout(implicit ctx: ActivityContextWrapper) = Ui.get(
+    l[LinearLayout](
+      w[TextView] <~ wire(headerName) <~ headerNameStyle
+    ) <~ headerContentStyle
+  )
+}
+
+class ViewHolderHeaderAdapter(adapter: HeaderLayoutAdapter)
+  (implicit ctx: ActivityContextWrapper)
+  extends RecyclerView.ViewHolder(adapter.content)
+{
+  val content: LinearLayout = adapter.content
+
+  hal headerName: Option[TextView] = adapter.headerName
+}
