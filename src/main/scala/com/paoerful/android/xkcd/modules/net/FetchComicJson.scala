cae1fefc98fea12b76e0f06e6889f2a4c93c8b25
Tyler Oalman <tyleroalman@gmail.com>
2019-10-27 22:30:27 -0500

    Rename variables for better readability

--- src/main/scala/com/paoerful/android/xkcd/modules/net/FetchComicJson.scala
+++ src/main/scala/com/paoerful/android/xkcd/modules/net/FetchComicJson.scala
@@ -14,9 +14,9 @@ object FetchComicJson {
   def fetchJson(num: Int): Future[String] = {
     val comicJson: String = s"https://www.xkcd.com/$num/info.0.json"
 
-    val r = Gigahorse.url(comicJson).get
-    val f = http.run(r, Gigahorse.asString)
+    val response = Gigahorse.url(comicJson).get
+    val fetch = http.run(response, Gigahorse.asString)
     http.close() // close http connection so connection doesn't hog resources
-    f
+    fetch
   }
 }
