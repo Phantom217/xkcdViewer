5eed4a970394f7b72a60a2a4e82977c53b1001c7
Tyler Oalman <tyleroalman@gmail.com>
2019-10-31 14:08:04 -0500

    Add module to parse json for data in key

---
+++ src/main/scala/com/paoerful/android/xkcd/modules/json/GetFields.scala
@@ -0,0 +1,27 @@
+package com.paoerful.android.modules.json
+
+import play.api.libs.json._
+
+import com.paoerful.android.modules.net.FetchComicJson._
+
+/** Utility for extracting contents of specific keys within JSON objects */
+object GetKeys {
+
+  val json = fetchJson(1312)
+  /** Get specified key from a json object
+    *
+    * @param key the target key in a json object
+    *
+    * pattern matching to parse the JsResult returned by `validate[String]`
+    * and return a `String` without the trailing `,`
+    * If the key does not exist in the json object, an error is thrown by
+    * `validate[String]`, handles error in the pattern matching and returns
+    * "Key not found", this should never happen unless key is mistyped.
+    */
+  def getKey(key: String): String =
+    (json \ s"$key").validate[String] match {
+      case JsSuccess(k, _) => k
+      case e: JsError      => "Key not found"
+    }
+
+}
--- src/main/scala/com/paoerful/android/xkcd/modules/net/FetchComicJson.scala
+++ src/main/scala/com/paoerful/android/xkcd/modules/net/FetchComicJson.scala
@@ -1,18 +1,23 @@
 package com.paoerful.android.modules.net
 
-import gigahorse._, support.okhttp.Gigahorse
-import scala.concurrent._
+import scala.concurrent._, duration._
 
+import gigahorse._, support.okhttp.Gigahorse
+import play.api.libs.json._
+
+/** Utility for fetching JSON object from xkcd */
 object FetchComicJson {
 
   /** Fetches the companion json of a given comic.
     *
     * @param num the comic number
     */
-  def fetchJson(num: Int): String =
+  def fetchJson(num: Int): JsValue =
     Gigahorse.withHttp(Gigahorse.config) { http =>
       val r = Gigahorse.url(s"https://xkcd.com/$num/info.0.json").get
-      val f = http.run(r, Gigahorse.asString)
+      val f = http.run(r, parse)
       Await.result(f, 120.seconds)
     }
+
+  private def parse = Gigahorse.asString andThen Json.parse
 }
